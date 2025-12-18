7b56fb137375a183afd4101eaa86320aae9abd29
ChandraVardhanReddy23 <chandravardhanreddy.d@gmail.com>
2025-12-18 09:44:39 +0530

    readtasks and writetasks functions

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+const fs=require('fs');
+const path=require('path');
+const filepath=path.join(__dirname,'task.json');
+function readTasks(){
+    JSON.parse(fs.readFileSync(filepath,'utf-8'));
+}
+function writeTasks(tasks){
+    JSON.parse(fs.writeFileSync(filepath,JSON.stringify(tasks,null,2)));
+}
